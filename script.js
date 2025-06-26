b2bafb79ae6d8abdb695a4f93633d64c87f5a77c
daniel johnson <dbjohn@umich.edu>
2025-06-26 16:06:30 -0400

    adding to github

---
+++ script.js
@@ -0,0 +1,18 @@
+// const openButton = document.querySelector(".open-modal");
+// const modal = document.querySelector(".modal-content");
+// const closeButton = document.querySelector(".close-modal");
+
+// function openModal() {
+//     modal.classList.remove("hide");
+// }
+
+// function closeModal(e, clickedOutside) {
+//     if (clickedOutside) {
+//         if (e.target.classList.contains("modal-content"))
+//             modal.classList.add("hide");
+//     } else modal.classList.add("hide");
+// }
+
+// openButton.addEventListener("click", openModal);
+// //modal.addEventListener("click", (e) => closeModal(e, true));
+// closeButton.addEventListener("click", closeModal);
